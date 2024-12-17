35c1e04c0d808584d6192fb0a49958653c876311
Rao-Muhammad-shayan <areesharao9@gmail.com>
2024-12-17 15:40:33 +0500

    add

---
+++ shayan.js
@@ -0,0 +1,17 @@
+// var a=prompt();
+// // console.log(prompt());
+// document.write( "<h1>"+a+"</h1>");
+var a=prompt();
+if (a !== null) {
+    document.write("<h1>nhi pehne ho na sb pata hai</h1>");
+} else if (a === "red") {
+    document.write("<h1>nhi pehne ho na sb pata hai</h1>");
+} else if (a === "green") {
+    document.write("<h1>nhi pehne ho na sb pata hai</h1>");
+}
+else if (a === "yellow") {
+    document.write("<h1>nhi pehne ho na sb pata hai</h1>");
+}
+else if (a === "blue") {
+    document.write("<h1>nhi pehne ho na sb pata hai</h1>");
+}
